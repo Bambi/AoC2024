4490742df0b344c82971480bfb81e8f655b14c72
Antoine Sgambato <176003+Bambi@users.noreply.github.com>
2025-08-06 09:09:06 +0200

    first puzzle part 1

---
+++ 1/1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using loc_c = std::vector<unsigned>;
+
+void parsing(loc_c &left, loc_c &right) {
+  for (std::string line; std::getline(std::cin, line);) {
+    std::istringstream iss(line);
+    unsigned l, r;
+    iss >> l >> r;
+    left.push_back(l);
+    right.push_back(r);
+  }
+}
+
+int main() {
+  auto left = loc_c();
+  auto right = loc_c();
+
+  parsing(left, right);  
+
+  sort(left.begin(), left.end());
+  sort(right.begin(), right.end());
+  // for (auto x: right) std::cout << x << std::endl;
+
+  unsigned res{};
+  for (unsigned i=0 ; i<left.size() ; i++)
+    res += std::abs(static_cast<int>(left[i]) - static_cast<int>(right[i]));
+
+  std::cout << "Distance: " << res << std::endl;
+  return 0;
+}
